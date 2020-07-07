c7ac5122eefd9ea57f71496acc9623bdbc0dd6da
Sean Oliver <snoliver@umich.edu>
2020-07-07 12:49:40 -0400

    made xml parser outline

--- data_collection.py
+++ data_collection.py
@@ -4,6 +4,20 @@ from io import BytesIO
 import os
 import xml.sax
 
+
+class VoteHandler(xml.sax.ContentHandler):
+    def __init__(self):
+        self.currentData = ""
+        self.FirstName = ""
+        self.LastName = ""
+        self.Party = ""
+        self.Constituency = ""
+        self.ProvinceTerritory = ""
+        self.IsVoteYea = False
+        self.IsVoteNay = False
+        self.IsVotePaired = False
+        self.DecisionAgreedTo = False
+
 def getVote(parliment, session, vote_num):
     URL = 'https://www.ourcommons.ca/Members/en/votes/' + str(parliment) + '/' \
         + str(session) + '/' + str(vote_num) + '/xml'
@@ -19,6 +33,17 @@ def getVote(parliment, session, vote_num):
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, 'w') as my_data_file:
         my_data_file.write(buffer.getvalue().decode('iso-8859-1'))
+
+    # create an XMLReader
+    parser = xml.sax.make_parser()
+    # turn off namepsaces
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+    # override the default ContextHandler
+    Handler = VoteHandler()
+    parser.setContentHandler( Handler )
+    
+    parser.parse(filename)
 
 
 def main():
